4a21972ca39006cfaab2894f817a85e4317bbaa0
mateocapon <mcapon@fi.uba.ar>
2023-03-28 18:06:11 -0300

    feat: bets loader counter

--- server/common/bets_loaded_counter.py
+++ server/common/bets_loaded_counter.py
@@ -1,14 +1,17 @@
 
 
 
-def count_loaded_bets(clients_queue, number_clients):
-
+def count_loaded_bets(clients_queue, load_bets_queue, waiting_winner_queue, number_clients):
+	while len(self.agencies_stored_bets) < self.number_clients:
+		client_sock, agency = load_bets_queue.get()
+	while True:
+			
 
 
 
 
             # Ver como contar hasta 5 entre hilos.
             # self.agencies_stored_bets.add(int(agency))
-            # if len(self.agencies_stored_bets) == self.number_clients:
+            # if :
             #     logging.info(f'action: sorteo | result: success')
             #     self.__send_winners()
